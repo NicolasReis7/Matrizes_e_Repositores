d8aab6e598c1b68325355419c4cdbeb4db8242a9
NicolasReis7 <nickinhoaugusto77@gmail.com>
2026-05-21 15:09:45 -0300

    Update exp08.c

--- exp08.c
+++ exp08.c
@@ -4,7 +4,6 @@ int main() {
 
     int vetor[5];
 
-    // Entrada dos números
     printf("Digite 5 numeros inteiros:\n");
 
     for(int i = 0; i < 5; i++) {
@@ -12,7 +11,6 @@ int main() {
         scanf("%d", &vetor[i]);
     }
 
-    // Exibe em ordem inversa
     printf("\nNumeros em ordem inversa:\n");
 
     for(int i = 4; i >= 0; i--) {
