8cf2475fccbd1cb676f873e15e02fae019c5f165
NicolasReis7 <nickinhoaugusto77@gmail.com>
2026-05-21 15:08:42 -0300

    Update exp05.c

--- exp05.c
+++ exp05.c
@@ -6,25 +6,21 @@ int main() {
     int numero;
     int encontrado = 0;
 
-    // Semente fixa
     srand(1);
 
-    // Preenche o vetor com números aleatórios
     for(int i = 0; i < 25; i++) {
         vetor[i] = rand() % 101;
     }
 
-    // Mostra o vetor
 
     for(int i = 0; i < 25; i++) {
         printf("%3d ", vetor[i]);
     }
 
-    // Pede um número ao usuário
+    
     printf("\n\nDigite um numero para buscar: ");
     scanf("%d", &numero);
 
-    // Procura o número no vetor
     for(int i = 0; i < 25; i++) {
         if(vetor[i] == numero) {
             printf("Numero encontrado na posicao %d\n", i);
