4c8b22052bdc2124525dce67741e8d060b262e6c
NicolasReis7 <nickinhoaugusto77@gmail.com>
2026-05-21 15:10:36 -0300

    Update exp13.c

--- exp13.c
+++ exp13.c
@@ -5,7 +5,6 @@ int main() {
     int numeros[10];
     int maior, menor;
 
-    // Entrada dos números
     printf("Digite 10 numeros inteiros:\n");
 
     for(int i = 0; i < 10; i++) {
@@ -13,18 +12,15 @@ int main() {
         printf("Numero %d: ", i + 1);
         scanf("%d", &numeros[i]);
 
-        // Inicializa maior e menor
         if(i == 0) {
             maior = numeros[i];
             menor = numeros[i];
         }
 
-        // Verifica maior
         if(numeros[i] > maior) {
             maior = numeros[i];
         }
 
-        // Verifica menor
         if(numeros[i] < menor) {
             menor = numeros[i];
         }
