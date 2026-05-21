05a6481e0731148b45c11ce8029e02e3ba8e046b
NicolasReis7 <nickinhoaugusto77@gmail.com>
2026-05-21 15:07:31 -0300

    Update exp03.c

--- exp03.c
+++ exp03.c
@@ -5,19 +5,15 @@ int main() {
     int vetor[25];
     int maior, posicao;
 
-    // Semente fixa
     srand(1);
 
-    // Preenche o vetor
     for(int i = 0; i < 25; i++) {
         vetor[i] = rand() % 101;
     }
 
-    // Inicializa maior e posição
     maior = vetor[0];
     posicao = 0;
 
-    // Encontra o maior valor e sua posição
     for(int i = 1; i < 25; i++) {
         if(vetor[i] > maior) {
             maior = vetor[i];
@@ -25,7 +21,6 @@ int main() {
         }
     }
 
-    // Mostra o vetor
     for(int i = 0; i < 25; i++) {
         printf("%3d ", vetor[i]);
     }
